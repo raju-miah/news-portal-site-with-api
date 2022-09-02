6572416971b8dfe7b3353bf5e528ea5796cf2a4f
raju-miah <rajumiahpollob7@gmail.com>
2022-09-03 01:23:46 +0600

    get the catagories array

--- js/api.js
+++ js/api.js
@@ -1,1 +1,11 @@
-console.log('added api js')
+const loadCaragories = () => {
+    fetch('https://openapi.programming-hero.com/api/news/categories')
+        .then(res => res.json())
+        .then(data => displayCatagories(data.data))
+};
+
+const displayCatagories = (cata) => {
+    console.log(cata)
+}
+
+loadCaragories();
