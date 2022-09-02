b8ad7ed58582defa67fef90e252821e9ca902ca4
raju-miah <rajumiahpollob7@gmail.com>
2022-09-03 02:32:39 +0600

    set catagotries name

--- js/api.js
+++ js/api.js
@@ -1,11 +1,20 @@
 const loadCaragories = () => {
     fetch('https://openapi.programming-hero.com/api/news/categories')
         .then(res => res.json())
-        .then(data => displayCatagories(data.data))
+        .then(data => displayCatagories(data.data.news_category))
 };
 
-const displayCatagories = (cata) => {
-    console.log(cata)
+const displayCatagories = (catagorie) => {
+    const catagorieContainer = document.getElementById('catagorie-container');
+    catagorie.forEach(catagories => {
+        const catagoriesDiv = document.createElement('div');
+        catagoriesDiv.innerHTML = `
+        <p class="p-3">${catagories.category_name}</p>
+        `;
+        catagorieContainer.appendChild(catagoriesDiv)
+        console.log(catagories)
+    });
 }
+
 
 loadCaragories();
